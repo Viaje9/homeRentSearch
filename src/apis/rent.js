b52ae6a128ccb25c88a5d696404b11162052af7d
Brain <h99y5246cv@gmail.com>
2024-08-22 16:48:20 +0800

    chore: Update getRentData API endpoint to use GET method with urlParams as query parameters

--- src/apis/rent.js
+++ src/apis/rent.js
@@ -1,14 +1,7 @@
 import axios from 'axios'
 
 export function getRentData(urlParams) {
-  return axios.post(
-    'https://script.google.com/macros/s/AKfycbyKWhxljamFvT1O0LiUbNuwLL8ywHylYbUjTPjHMw4TmF45CIA8ol7hOuRofCXIJ1P0/exec',
-    urlParams,
-    {
-      headers: {
-        'Content-Type': 'text/plain;charset=utf-8'
-      },
-      redirect: 'follow'
-    }
-  )
+  return axios.get('https://us-central1-home-rent-backend.cloudfunctions.net/getRentListApi', {
+    params: { urlParams }
+  })
 }
